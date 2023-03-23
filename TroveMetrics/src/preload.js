b68b91f6cc4e6f31d46503f2be13c067d08acbc0
Sam Henderson <shendo87@gmail.com>
2023-03-23 14:50:31 -0700

    temp

--- TroveMetrics/src/preload.js
+++ TroveMetrics/src/preload.js
@@ -2,6 +2,7 @@
 // https://www.electronjs.org/docs/latest/tutorial/process-model#preload-scripts
 
 const { contextBridge, ipcRenderer } = require('electron')
+const electronReload = require('electron-reload')
 
 const channels = {
   frontend: { 
