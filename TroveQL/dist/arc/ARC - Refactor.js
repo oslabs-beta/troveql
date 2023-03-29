da6beac348a9c245ed3c2614f2c436bb7c2a4d35
a-t-klein <alxtkln@gmail.com>
2023-03-28 18:08:56 -0700

    refactor arc

--- TroveQL/dist/arc/ARC - Refactor.js
+++ TroveQL/dist/arc/ARC - Refactor.js
@@ -1,5 +1,9 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
+const cacheItem_1 = __importDefault(require("./cacheItem"));
 class TroveCache {
     constructor(size) {
         this.get = (query) => {
@@ -23,22 +27,53 @@ class TroveCache {
                     console.log('In Get Case III');
                     return { result: false, miss: 'b2' };
                     break;
-                case this.b2.has(query):
+                case !this.t1.has(query) &&
+                    this.t2.has(query) &&
+                    this.b1.has(query) &&
+                    this.b2.has(query):
                     console.log('In Get Case III');
                     return { result: false, miss: 'miss' };
                     break;
             }
         };
         this.set = (res) => {
+            const node = new cacheItem_1.default(res.val);
             switch (true) {
                 case res.miss === 'b1':
                     console.log('In Set Case II');
+                    this.adaptation(true);
+                    this.replace(false);
+                    //add data to cache
                     break;
                 case res.miss === 'b2':
                     console.log('In Set Case III');
+                    this.adaptation(false);
+                    this.replace(true);
+                    //add data to cache
                     break;
                 case res.miss === 'miss':
                     console.log('In Set Case III');
+                    const l1 = this.t1.size + this.t2.size;
+                    switch (true) {
+                        case l1 === this.capacity:
+                            if (this.t1.size < this.capacity) {
+                                //remove RRU
+                                //Replace
+                            }
+                            else {
+                                //remove LRU from this.t1
+                            }
+                            break;
+                        case l1 < this.capacity:
+                            const totalSize = this.t1.size + this.b1.size + this.t2.size + this.b2.size;
+                            if (totalSize >= this.capacity) {
+                                if (totalSize === this.capacity * 2) {
+                                    //remove LRU in B2
+                                }
+                                this.replace(false);
+                            }
+                            break;
+                    }
                     break;
             }
         };
--- TroveQL/src/arc/ARC - Refactor.ts
+++ TroveQL/src/arc/ARC - Refactor.ts
@@ -27,7 +27,7 @@ class TroveCache {
         const result = cache.get(query) as ItemType; //this may be an issue
         cache.delete(query);
         this.t2.set(query, result);
-        
+
         return {
           result: result.value,
           miss: false,
@@ -44,7 +44,10 @@ class TroveCache {
         return { result: false, miss: 'b2' };
         break;
 
-      case this.b2.has(query):
+      case !this.t1.has(query) &&
+        this.t2.has(query) &&
+        this.b1.has(query) &&
+        this.b2.has(query):
         console.log('In Get Case III');
         return { result: false, miss: 'miss' };
         break;
@@ -52,16 +55,47 @@ class TroveCache {
   };
 
   set = (res: ResponseType) => {
+    const node = new CacheItem(res.val);
+
     switch (true) {
       case res.miss === 'b1':
         console.log('In Set Case II');
+        this.adaptation(true);
+        this.replace(false);
+        //add data to cache
         break;
 
       case res.miss === 'b2':
         console.log('In Set Case III');
+        this.adaptation(false);
+        this.replace(true);
+        //add data to cache
         break;
       case res.miss === 'miss':
         console.log('In Set Case III');
+        const l1: number = this.t1.size + this.t2.size;
+
+        switch (true) {
+          case l1 === this.capacity:
+            if (this.t1.size < this.capacity) {
+              //remove RRU
+              //Replace
+            } else {
+              //remove LRU from this.t1
+            }
+            break;
+          case l1 < this.capacity:
+            const totalSize =
+              this.t1.size + this.b1.size + this.t2.size + this.b2.size;
+            if (totalSize >= this.capacity) {
+              if (totalSize === this.capacity * 2) {
+                //remove LRU in B2
+              }
+              this.replace(false);
+            }
+            break;
+        }
+
         break;
     }
   };
