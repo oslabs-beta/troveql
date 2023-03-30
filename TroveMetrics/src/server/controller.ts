554a6622f19fe052a71879fcfd92e51955006dfa
Sam Henderson <shendo87@gmail.com>
2023-03-29 17:12:18 -0700

    Ready to push after merge

--- TroveMetrics/src/server/controller.ts
+++ TroveMetrics/src/server/controller.ts
@@ -10,18 +10,22 @@ type controller = {
 const troveController: controller = {
 
   post: function(req: Request, res: Response, next: NextFunction) {
-
     // Pull from local data, add new data, and write back
     fs.readFile(path.join(TroveQLPath, 'metrics.json'), "utf-8")
     .then(data => JSON.parse(data))
     .then(parsedData => {
 
       // Translate Hit / Miss into data format ready for Chart.js
-      let hitOrMiss: string = 'MISS';
-      if (req.body.cacheHit) hitOrMiss = 'HIT';
-      parsedData.cache[hitOrMiss] += 1;
-      parsedData.queries.push(req.body)
-      
+      if (req.body.cacheHit === undefined) {
+        parsedData.cache['HIT'] = 0;
+        parsedData.cache['MISS'] = 0;
+        parsedData.queries = [];
+      } else {
+        let hitOrMiss: string = req.body.cacheHit ? 'HIT' : 'MISS';
+        parsedData.cache[hitOrMiss] += 1;
+        parsedData.queries.push(req.body)
+      }
+
       // Send file data back to server to pass on to Renderer
       res.locals.data = parsedData
 
