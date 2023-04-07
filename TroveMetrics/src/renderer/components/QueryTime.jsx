f46253637bf95606db2620a39b21ba506943fbd8
erikahjung <erika.h.jung@gmail.com>
2023-04-06 17:13:09 -0700

    queryTime line chart functional

--- TroveMetrics/src/renderer/components/QueryTime.jsx
+++ TroveMetrics/src/renderer/components/QueryTime.jsx
@@ -1,11 +1,63 @@
 import * as React from 'react';
+import { Line } from 'react-chartjs-2';
+import variables from '../styles/_variables.module.scss'
+
+// what should the time be for mutations? currently it's null
+// tooltip for the query?
 
 function QueryTime ({ queries }) {
-  const queryCount = queries.length;
-  // console.log('QueryTime cacheData: ', queries[queryCount - 1].queryTime);
+  const queryTimeData = [0];
+  queries.forEach((queryObj) => queryTimeData.push(queryObj.queryTime));
+
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        display: false,
+      },
+      tooltip: {
+        callbacks: {
+          // title: (xDatapoint) => formatXValue(xDatapoint.raw),
+          label: (context) => '',
+        }
+      }
+    },
+    scales: {
+      x: {
+        title: {
+          display: true,
+          text: 'Query'
+        }
+      },
+      y: {
+        title: {
+          display: true,
+          text: 'Time (milliseconds)'
+        }
+      },
+    }
+  };
+
+  const data = {
+    labels: Array.from(queryTimeData.keys()),
+    datasets: [
+      {
+        label: 'Query Time (ms)',
+        data: queryTimeData,
+        borderColor: variables.orange
+      }
+    ]
+  }
 
   return (
-    <p>QueryTime: {queries[queryCount - 1].queryTime}</p>
+    <div className="wide-container">
+      <div className="chart-header">
+        <h3>Query Response Time</h3>
+      </div>
+      <div className="chart-cont">
+        <Line options={options} data={data}/>
+      </div>
+    </div>
   )
 }
 
