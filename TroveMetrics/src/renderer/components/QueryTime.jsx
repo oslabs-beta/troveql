e75d7f3fa53251cec15162fd80a0ba6578e1a351
erikahjung <erika.h.jung@gmail.com>
2023-04-06 19:29:27 -0700

    converting queryTime line graph to a bar chart

--- TroveMetrics/src/renderer/components/QueryTime.jsx
+++ TroveMetrics/src/renderer/components/QueryTime.jsx
@@ -1,13 +1,16 @@
 import * as React from 'react';
-import { Line } from 'react-chartjs-2';
+import { Bar } from 'react-chartjs-2';
 import variables from '../styles/_variables.module.scss'
+import { query } from 'express';
 
 // what should the time be for mutations? currently it's null
 // tooltip for the query?
 
 function QueryTime ({ queries }) {
   const queryTimeData = [0];
-  queries.forEach((queryObj) => queryTimeData.push(queryObj.queryTime));
+  queries.forEach((queryObj) => {
+    queryTimeData.push(queryObj.queryTime);
+  });
 
   const options = {
     responsive: true,
@@ -17,8 +20,16 @@ function QueryTime ({ queries }) {
       },
       tooltip: {
         callbacks: {
-          // title: (xDatapoint) => formatXValue(xDatapoint.raw),
-          label: (context) => '',
+          title: (context) => '',
+          footer: (context) => 'Query String: ' + queries[context[0].dataIndex].query,
+          afterFooter: (context) => {
+            const queryVariables = queries[context[0].dataIndex].variables;
+            let variablesStr = '';
+            for (const key in queryVariables) {
+              variablesStr += key + ':' + queryVariables[key]
+            }
+            return 'Query Variables: ' + variablesStr;
+          },
         }
       }
     },
@@ -32,7 +43,7 @@ function QueryTime ({ queries }) {
       y: {
         title: {
           display: true,
-          text: 'Time (milliseconds)'
+          text: 'Response Time (milliseconds)'
         }
       },
     }
@@ -42,9 +53,9 @@ function QueryTime ({ queries }) {
     labels: Array.from(queryTimeData.keys()),
     datasets: [
       {
-        label: 'Query Time (ms)',
+        label: 'Response Time (ms)',
         data: queryTimeData,
-        borderColor: variables.orange
+        backgroundColor: variables.orange
       }
     ]
   }
@@ -52,10 +63,10 @@ function QueryTime ({ queries }) {
   return (
     <div className="wide-container">
       <div className="chart-header">
-        <h3>Query Response Time</h3>
+        <h3>Query Response Times</h3>
       </div>
       <div className="chart-cont">
-        <Line options={options} data={data}/>
+        <Bar data={data} options={options} />
       </div>
     </div>
   )
